db17d2d2f9ff9fe5a6b6c6a68b4c372960d09d28
sibamohanty <mohanty.siba@gmail.com>
2015-04-23 18:15:05 +0530

    with bottom date panel hover in and out enabled

---
+++ static/js/date_panel_ko.js
@@ -0,0 +1,68 @@
+
+(function (){
+    function blah (month,year) {
+                 return new Date(year, month, 0).getDate();
+    }
+    function DatePanelViewModel(){
+        // The data here is system generated
+        // ensure that the system clock is uptodate
+        
+	var self = this;
+	self.selectedDay = ko.observable();
+        self.daysInMonth = function (month,year){
+	    var dt = new Date();
+	    console.log(dt.getMonth());
+
+	    self.month = month || dt.getMonth()+1;
+	    console.log("The current month is "+ month);
+	    self.year = year || dt.getYear();
+            return new Date(self.year, self.month, 0).getDate();
+
+
+	},
+	self.init = function (){
+            self.noofdays= self.daysInMonth();
+            console.log(self.noofdays);
+            self.days= ko.observableArray();
+            for (var i=1; i<= self.noofdays; i++){
+		var eff = self.randomEfficiencyForTheDay();
+
+                self.days.push({day: i, cop: eff});
+            }
+	},
+	self.selectDate = function (day, e){
+	    self.showData(self.year,self.month,day.day);
+	    console.log(e);
+	    $(e.target).css('background-color',"yellow");
+	},
+	self.randomEfficiencyForTheDay = function (){
+	   return Math.random(.1);
+	},
+	self.copColor = ko.computed(function (){
+	// returns the color as per the efficiency level;
+		return self.cop;
+	},this );
+
+	self.showData = function (y,m,d){
+	    // Will check for existing data, and will fetch if it's not there
+	    //
+	    console.log("year"+y+" month"+m+" day"+d);
+
+	    // And pass it to the appropriate viewmodel.
+	},
+	self.logMouseOver = function (d,e){
+
+	    $(e.target).css("background-color","blue");
+	    console.log(d.cop);
+	},
+	self.logMouseOut = function (d,e){
+	   $(e.target).css("background-color", "transparent");
+
+	}
+        this.init();
+    }
+    ko.applyBindings(
+        new DatePanelViewModel()
+      );
+
+})();
